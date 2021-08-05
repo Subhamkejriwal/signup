86f80685148f29df13f1e527518a3c285d796ca0
Subhamkejriwal <88478831+Subhamkejriwal@users.noreply.github.com>
2021-08-05 14:57:56 +0530

    Add files via upload

---
+++ User_Info.sql
@@ -0,0 +1,11 @@
+create table User_Info
+(
+sl_no number(3)  foreign key refernces events&schedule(sl_no),
+First Name varchar(50) primary key,
+Last Name varchar(50),
+Email varchar(50),
+Password varchar(50)
+ 
+;
+
+ 
